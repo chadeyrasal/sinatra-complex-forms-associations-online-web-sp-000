727cd0673e76288b6690f1a99bfbde77406199f7
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-03-07 19:25:12 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,7 +30,7 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do
+  post '/pets/:id' do
     if !params[:pet].keys.include?("owner_id")
       params[:pet]["owner_id"] = []
     end
