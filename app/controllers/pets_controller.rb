9ae633a2a882c41c8bd1631821a04252a1d34c62
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-03-07 19:32:55 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,49 @@
+#class PetsController < ApplicationController
+
+#  get '/pets' do
+#    @pets = Pet.all
+#    erb :'/pets/index'
+#  end
+
+#  get '/pets/new' do
+#    @owners = Owner.all
+#    erb :'/pets/new'
+#  end
+
+#  post '/pets' do
+#    @pet = Pet.create(params["pet"])
+#    if !params["owner"]["name"].empty?
+#      @pet.owner = Owner.create(name: params["owner"]["name"])
+#    end
+#    @pet.save
+#    redirect to "pets/#{@pet.id}"
+#  end
+
+#  get '/pets/:id/edit' do
+#    @pet = Pet.find(params[:id])
+#    @owners = Owner.all
+#    erb :'/pets/edit'
+#  end
+
+#  get '/pets/:id' do
+#    @pet = Pet.find(params[:id])
+#    erb :'/pets/show'
+#  end
+
+#  post '/pets/:id' do
+#    @pet = Pet.find(params[:id])
+#    @pet.update(params["pet"])
+#    if !params["owner"]["name"].empty?
+#      @pet.owner = Owner.create(name: params["owner"]["name"])
+#    end
+#    @pet.save
+#    redirect to "pets/#{@pet.id}"
+#  end
+
+#end
+
+
+
 class PetsController < ApplicationController
 
   get '/pets' do
@@ -6,23 +52,16 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do
-    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do
-    @pet = Pet.create(params["pet"])
+    @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save
-    redirect to "pets/#{@pet.id}"
-  end
-
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    @owners = Owner.all
-    erb :'/pets/edit'
+    redirect "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do
@@ -37,7 +76,11 @@ class PetsController < ApplicationController
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save
-    redirect to "pets/#{@pet.id}"
+    redirect "pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 end
